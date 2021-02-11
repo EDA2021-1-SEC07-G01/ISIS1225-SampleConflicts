0aa613fa2d0c76dc80b0e1add3775768f653ee45
angela diazgranados <a.diazgranados@uniandes.edu.co>
2021-02-10 22:52:02 -0500

    "cambios en main"

--- App/view.py
+++ App/view.py
@@ -76,7 +76,7 @@ while True:
         print("Cargando información de libros....")
         books = loadBooks()
         print('Total de libros cargados: ' + str(lt.size(books)))
-
+        print(" 3- Cargar Book-Tags!")
         # TO-DO: Modificación de Est-1 en el Lab 2
 
 
